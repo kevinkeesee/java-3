396b21a5f331801947975b2d2ca99a4333d53341
Kevin Keesee <64923372+kevinkeesee@users.noreply.github.com>
2020-07-05 20:18:08 -0500

    attempted exercise 2

--- src/lambda_streams/Animals.java
+++ src/lambda_streams/Animals.java
@@ -8,9 +8,29 @@ class Animals {
 
     public static void main(String[] args) {
         System.out.println("original animals : " + animals);
+        
+        LambdaSort lambdaCaps = (animaList, mutate) -> {
+        	return capsFirst(animaList, mutate);
+        };
+        
+        LambdaSort lambdaLower = (animaList, mutate) -> {
+        	return lowerFirst(animaList, mutate);
+        };
+        
+        LambdaAdd lambdaAdd = (animal) -> {
+        	return addAnimal(animal);
+        };
+        
+        LambdaArrange lambdaFlip = (mutate) -> {
+        	return flipAnimals(mutate);
+        };
+        
+        LambdaArrange lambdaSort = (mutate) -> {
+        	return sortAnimals(mutate);
+        };
 
         //clean up the animals array by using the capsFirst() method. follow instructions in the method definition. 
-        List<String> cleaned = capsFirst(animals, false);
+        List<String> cleaned = lambdaCaps.sort(animals, false);
         System.out.println(cleaned);
        
         //do not modify these addAnimal() method invocations
@@ -20,43 +40,102 @@ class Animals {
         addAnimal("lEOpArD");
         //---------------------------------------
 
-        capsFirst(animals,true);
+        lambdaCaps.sort(animals, true);
         System.out.println(animals);
 
-        List<String> lowered = lowerFirst(animals,false);
+        List<String> lowered = lambdaLower.sort(animals, false);
         System.out.println(lowered);
-
-        
-
     }
 
     static List<String> capsFirst(List<String> animaList, boolean mutate) {
-        //clean up the animals list so that the first letter is capitalized, and all the other letters are lowercased. Use a stream to accomplish this task.  Also, the 2nd parameter of this function is a boolean.  use this boolean 'flag' to determine whether or not to 'mutate' the original animals array stored as a static class field.  if the flag is set to 'true', mutate the animals and return the animals out of the function.  if it is false, create a copy of the animals, perform your stream operations on the copy, and return the copy of animals out of the function, WITHOUT modifying the original animals array.  
-        return animals;
+        //clean up the animals list so that the first letter is capitalized, and all the other letters are lowercased. Use a stream to accomplish this task.  Also, the 2nd parameter of this function is a boolean.  use this boolean 'flag' to determine whether or not to 'mutate' the original animals array stored as a static class field.  if the flag is set to 'true', mutate the animals and return the animals out of the function.  if it is false, create a copy of the animals, perform your stream operations on the copy, and return the copy of animals out of the function, WITHOUT modifying the original animals array.
+    	if (mutate) {
+    		for(int i = 0; i < animaList.size(); i++) {
+        		animaList.set(i, animaList.get(i).toLowerCase());
+    			char[] array = animaList.get(i).toCharArray();
+        		array[0] = Character.toUpperCase(array[0]);
+        		animals.set(i, new String(array));
+        	}
+    		return animals;
+    	} else {
+    		List<String> copy = new ArrayList<String>(animaList);
+    		for(int i = 0; i < copy.size(); i++) {
+    			copy.set(i, copy.get(i).toLowerCase());
+    			char[] array = copy.get(i).toCharArray();
+    			array[0] = Character.toUpperCase(array[0]);
+    			copy.set(i, new String(array));
+    		}
+    		return copy;
+    	}
     }
 
     static String addAnimal(String animal) {
         //add an animal to the animal list.
+    	List<String> copy = new ArrayList<String>(animals);
+    	copy.add(animal);
+    	animals = copy;
         return animal;
     };
 
-
     static List<String> lowerFirst(List<String> animaList, boolean mutate) {
         //lowercase the first letter, and uppercase the rest of the letters, using streams.  Also, depending on the value of the boolean flag 'mutate', mutate the original animals list, or perform your stream operations on a 'copy' of the animals list.  return the list out of hte function in both cases.
-        return animaList;
+    	if (mutate) {
+    		for(int i = 0; i < animaList.size(); i++) {
+        		animaList.set(i, animaList.get(i).toUpperCase());
+    			char[] array = animaList.get(i).toCharArray();
+        		array[0] = Character.toLowerCase(array[0]);
+        		animals.set(i, new String(array));
+        	}
+    		return animals;
+    	} else {
+    		List<String> copy = new ArrayList<String>(animaList);
+    		for(int i = 0; i < copy.size(); i++) {
+    			copy.set(i, copy.get(i).toUpperCase());
+    			char[] array = copy.get(i).toCharArray();
+    			array[0] = Character.toLowerCase(array[0]);
+    			copy.set(i, new String(array));
+    		}
+    		return copy;
+    	}
     }
 
     static List<String> flipAnimals(boolean mutate) {
         //reverse the order of the animals in the animal list.  If the booleaen parameter is true, reverse the static animals array list by mutating the array.  if the mutate boolean is false, flip a 'copy' of the animals list, then return that list of flipped animals, WITHOUT mutating the static animals array. return the flipped list in both cases.
-
-        List<String> instructions = Arrays.asList("Flip","the","animals","list","."," ","Mutate","the","animals","only","if","flag","true");
-        return instructions;
+    	if(mutate) {
+    		Collections.reverse(animals);
+    		return animals;
+    	} else {
+    		List<String> copy = new ArrayList<String>(animals);
+    		Collections.reverse(copy);
+    		return copy;
+    	}
     }
 
     static List<String> sortAnimals(boolean mutate) {
-    //sort the animals in alphabetical order.  If the booleaen parameter is true, mutating the animals list.  if the mutate boolean is false, sort a 'copy' of the animals list, then return that list of sorted animals, WITHOUT mutating the static animals array. return the sorted list in both cases.
-    return animals;
+    	//sort the animals in alphabetical order.  If the booleaen parameter is true, mutating the animals list.  if the mutate boolean is false, sort a 'copy' of the animals list, then return that list of sorted animals, WITHOUT mutating the static animals array. return the sorted list in both cases.
+    	if (mutate) {
+    		animals.sort(Comparator.comparing(String::toString));
+    		return animals;
+    	} else {
+    		List<String> copy = new ArrayList<String>(animals);
+    		copy.sort(Comparator.comparing(String::toString));
+    		return copy;
+    	}
     }
-
-
 }
+
+
+@FunctionalInterface
+interface LambdaSort {
+	List<String> sort(List<String> animaList, boolean mutate);
+}
+
+interface LambdaAdd {
+	String add(String animal);
+}
+
+interface LambdaArrange {
+	List<String> arrange(boolean mutate);
+}
+
+
--- src/lambda_streams/Numbers.java
+++ src/lambda_streams/Numbers.java
@@ -1,5 +1,6 @@
 package lambda_streams;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -29,63 +30,182 @@ class Numbers {
         etc...
 
         */
-
+        
+        LambdaCalcReInt lambdaAppend = (num) -> {
+        	return append(num);
+        };
+        
+        System.out.println(lambdaAppend.calc(55));
+        System.out.println(nums);
+        
+        LambdaCalcInt lambdaOdd = (num) -> {
+        	return isOdd(num);
+        };
+        
+        LambdaCalcInt lambdaEven = (num) -> {
+        	return isEven(num);
+        };
+        
+        lambdaOdd.calc(6);
+        lambdaEven.calc(1);
+        
+        LambdaCalcInt lambdaPrime = (num) -> {
+        	return isPrime(num);
+        };
+        
+        lambdaPrime.calc(3);
+        lambdaPrime.calc(4);
+        
+        LambdaCalcNull lambdaAdded = () -> {
+        	return added();
+        };
+        
+        System.out.println(lambdaAdded.calc());
+        
+        LambdaCalcNull lambdaSubtracted = () -> {
+        	return subtracted();
+        };
+        
+        LambdaCalcNull lambdaMultiplied = () -> {
+        	return multiplied();
+        };
+        
+        System.out.println(lambdaMultiplied.calc());
+        
+        LambdaCalcNull lambdaDivided = () -> {
+        	return divided();
+        };
+        
+        LambdaCalcNull lambdaMax = () -> {
+        	return findMax();
+        };
+        
+        System.out.println(lambdaMax.calc());
+        
+        LambdaCalcNull lambdaMin = () -> {
+        	return findMin();
+        };
+        
+        System.out.println(lambdaMin.calc());
+        
+        LambdaCalcInts lambdaCompare = (num1, num2) -> {
+        	return compare(num1, num2);
+        };
+        
     }
 
     static boolean isOdd(int i) {
         //determine if the value at the index i is odd.  return true if yes, return false if  no.
+    	if(nums.get(i) % 2 == 1) {
+    		System.out.println(nums.get(i) + " is odd.");
+    		return true;
+    	}
+    	System.out.println(nums.get(i) + " is even.");
         return false;
     }
 
     static boolean isEven(int i) {
         //determine if the value at the index i is even.  return true if yes, return false if  no.
+    	if(nums.get(i) % 2 == 0) {
+    		System.out.println(nums.get(i) + " is even.");
+    		return true;
+    	}
+    	System.out.println(nums.get(i) + " is odd.");
         return false;
     }
 
     static boolean isPrime(int i) {
          //determine if the value at the index i is a prime number.  return true if yes, return false if no.
-         return false;
+    	if(nums.get(i) <= 1) {
+    		System.out.println(nums.get(i) + " is too small.");
+    		return false;
+    	}
+    	
+    	for (int n = 2; n < nums.get(i); n++) {
+    		if (nums.get(i) % n == 0) {
+    			System.out.println(nums.get(i) + " is not prime.");
+    			return false;
+    		}
+    	}
+    	System.out.println(nums.get(i) + " is prime.");
+    	return true;
     }
 
     static int added() {
-        //add all the elements in the list.  return the sum.  
-        return 0;
+        //add all the elements in the list.  return the sum.
+        return nums.stream().mapToInt(Integer::intValue).sum();
     }
 
     static int subtracted() {
         //subtract all the elements in the list. return the remainder.
-        return 0;
+    	return nums.stream().mapToInt(Integer::intValue).sum() * -1;
     }
 
-    static int multipled() {
+    static int multiplied() {
         //multiply all the elements in the list. and return the product.
-        return 0;
+    	int product = 1;
+    	
+    	for (int i = 0; i < nums.size(); i++) {
+    		product *= nums.get(i);
+    	}
+        return product;
     }
 
     static int divided() {
         //multiply all the elements in the list. and return the product.
-        return 0;
+    	int product = 1;
+    	
+    	for (int i = 0; i < nums.size(); i++) {
+    		product /= nums.get(i);
+    	}
+        return product;
     }
 
     static int findMax() {
          //return the maximum value in the list.
-        return 0;
+        return nums.stream().max(Integer::compare).get();
     }
 
     static int findMin() {
         //return the minimum value in the list.
-        return 0;
+    	return nums.stream().min(Integer::compare).get();
     }
 
     static int compare(int i, int j) {
         //compare the values stored in the array at index position i and j.  
         //if the value at i is greater, return 1.  if the value at j is greater, return -1.  if the two values are equal, return 0.
+    	if(nums.get(i) > nums.get(j)) {
+    		return 1;
+    	} else if (nums.get(i) < nums.get(j)) {
+    		return -1;
+    	}
+    	
         return 0;
     }
 
     static int append(int n) {
         //add a new value to the values list. return that value after adding it to the list.
-        return 0;
+    	List<Integer> addedNums = new ArrayList<Integer>(nums);
+    	addedNums.add(n);
+    	nums = addedNums;
+    	return n;
     }
-
 }
+
+@FunctionalInterface
+interface LambdaCalcInt {
+	boolean calc(int num);
+}
+
+interface LambdaCalcReInt {
+	int calc(int num);
+}
+
+interface LambdaCalcInts {
+	int calc(int num1, int num2);
+}
+
+interface LambdaCalcNull {
+	int calc();
+}
+
